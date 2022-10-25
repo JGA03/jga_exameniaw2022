e5e8b12b2f9d6c8c1c808b19443efd0af26e1526
Javier García Aguilera <javieralumno1asir@gmail.com>
2022-10-25 12:26:15 +0200

    Cuarto commit
    el mejor git del mundo 2

--- jga_exameniaw2022.php
+++ jga_exameniaw2022.php
@@ -3,6 +3,7 @@
   <link href="style.css" rel="stylesheet" type="text/css">
  </head>
  <body>
+<img src="jgaimagen.jpg">
   <h1>Javier García</h1>
   <h2>25/10/2022</h2>
   <h3>repositorio, sirve para guardar tu trabajo</h3>
